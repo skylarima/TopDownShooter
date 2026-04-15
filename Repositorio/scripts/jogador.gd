ed87099ab207927ac041e95833ff7b9830f538ef
skylarima <mariajvh2010@gmail.com>
2026-04-15 16:48:33 -0300

    jogo top da mj
    balu é o dono desse jogo

--- Repositorio/scripts/jogador.gd
+++ Repositorio/scripts/jogador.gd
@@ -5,6 +5,8 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var direction = Vector2(0,0)
 
+@export var projetil: PackedScene
+
 func _physics_process(delta: float) -> void:
 	
 	rotacionar_corpo()
@@ -13,6 +15,13 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("atirar"):
+		disparar()
+
+func disparar():
+	var nova_bala = projetil.instantiate()
+	nova_bala.global_position = $EmissorBala
 func mover():
 	
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
