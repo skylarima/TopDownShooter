62d85986542c0cce4c2ec47f773829a7b81eadd0
skylarima <mariajvh2010@gmail.com>
2026-04-30 16:54:31 -0300

    balububu goat

--- Repositorio/scripts/bala.gd
+++ Repositorio/scripts/bala.gd
@@ -6,3 +6,9 @@ var direcao = Vector2.ZERO
 func _process(delta: float) -> void:
 	
 	position += velocidade * delta * direcao
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("inimigos"):
+		body.tomar_dano()
+		self.queue_free()
--- Repositorio/scripts/inimigo.gd
+++ Repositorio/scripts/inimigo.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
+@export var SPEED = 200.0
+
 @onready var jogador = %jogador
 
-const SPEED = 300.0
 
 func _physics_process(delta: float) -> void:
-	if jogador:
+	
+	if %jogador:
 		mover()
 	
 	move_and_slide()
@@ -14,3 +16,7 @@ func mover():
 	var direcao_jogador = (jogador.global_position - self.global_position).normalized()
 	
 	velocity = SPEED * direcao_jogador 
+	
+func tomar_dano():
+	
+	self.queue_free()
